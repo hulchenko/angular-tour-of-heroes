d4917d8761594317c8318f547f36801ff77ee763
Vadym Hulchenko <vhulchenko@flyht.com>
2022-04-21 15:59:55 -0600

    add list of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 import { AppComponent } from '../app.component'
 
 
@@ -9,7 +10,7 @@ import { AppComponent } from '../app.component'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'Vadym'
